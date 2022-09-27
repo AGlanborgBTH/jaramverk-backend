67c4fb0131071bd2b653577613ca69b4d5ba076c
AGlanborgBTH <93671007+AGlanborgBTH@users.noreply.github.com>
2022-09-27 13:20:36 +0200

    socket.io
    Implemented socket.io

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 require("dotenv").config()
 
 const express = require("express");
+const bodyParser = require('body-parser');
 const cors = require("cors");
 const morgan = require("morgan");
-const app = express();
 
-const { mwindex } = require("./middleware/index");
+const app = express();
+const httpServer = require("http").createServer(app);
 
 const doc = require("./routes/doc");
 const docPost = require("./routes/docPost");
@@ -13,19 +14,57 @@ const docPut = require("./routes/docPut")
 
 const port = process.env.PORT || 8082;
 
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+const { mwindex } = require("./middleware/index");
+
 app.use(cors());
 app.options('*', cors());
 app.disable('x-powered-by');
 app.use(express.json())
-app.use(morgan("combined"));
-
 app.use(mwindex);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== 'test') {
+    app.use(morgan('combined'));
+}
+
+io.on('connection', function (socket) {
+    console.log("User connected with socket id:", socket.id)
+
+    documents = {}
+
+    socket.on("doc", function (doc) {
+        documents[doc.id] = doc
+
+        socket.broadcast.emit("doc", doc);
+    })
+
+    socket.on("newDoc", function (doc) {
+        console.log(documents)
+        socket.emit("doc", documents[doc.id])
+    })
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected with socket id:", socket.id);
+    });
+});
+
+io.on("connect_error", (err) => {
+    console.log(`connect_error due to ${err.message}`);
+});
 
 app.use("/docs", doc)
 app.use("/docs/post", docPost);
 app.use("/docs/put", docPut)
 
-var server = app.listen(port, function () {
+var server = httpServer.listen(port, function () {
     var host = server.address().address;
     console.log('App listening at', host, port);
 });
